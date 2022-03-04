b83db9ebb6da1332804c64f07e3dd4341e45eb20
KonstantinosKoulaxizis <kkoulaxizis07@gmail.com>
2022-03-04 03:20:56 +0100

    remove checks at inputComponent

--- src/Components/InputComponent/index.tsx
+++ src/Components/InputComponent/index.tsx
@@ -11,16 +11,12 @@ const InputComponent: FunctionComponent<InputFieldModel> = ({ searchValue, setSe
   const debouncedStoreData = useDebounce(searchValue, 800)
 
   const onHandleSearchValue = (e: React.FormEvent<HTMLInputElement>) => {
-    if (e?.currentTarget?.value) {
-      setSearchValue(e.currentTarget.value)
-    }
+    setSearchValue(e.currentTarget.value)
   }
 
   useEffect(() => {
-    if (debouncedStoreData?.length > 2) {
-      MuseumRequests.getMuseumCollection()
-      // set search page to 1
-    }
+    MuseumRequests.getMuseumCollection()
+    // set search page to 1
   }, [debouncedStoreData])
 
   return (
